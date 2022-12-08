81eb331dabab43bf8eb95db81a5df36fab2f7a1e
Rene Reede <rene.reede@gmail.com>
2022-12-08 14:45:27 +0200

    Added Expense Items with props
    - Cleaned up code
    - Added Expense items with props
    - Created  seperate Expensedate item component

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2022, 2, 28),
+    },
+    {
+      id: "e2",
+      title: "TV",
+      amount: 492.47,
+      date: new Date(2022, 2, 28),
+    },
+    {
+      id: "e3",
+      title: "Playstation 5: God of War Edition",
+      amount: 994.67,
+      date: new Date(2022, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "Toaster",
+      amount: 42.42,
+      date: new Date(2022, 2, 28),
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
